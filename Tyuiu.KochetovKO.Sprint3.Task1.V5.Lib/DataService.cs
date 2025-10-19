0afd83481d600b11dc92d179fa287d83e1edb291
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-10-19 15:51:56 +0500

    Task1V5

---
+++ Tyuiu.KochetovKO.Sprint3.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KochetovKO.Sprint3.Task1.V5.Lib
+{
+    public class DataService : ISprint3Task1V5
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double multiValue = 1;
+            double x = 0.1;
+            while (startValue <= stopValue)
+            {
+                multiValue = multiValue + Math.Cos(x) + Math.Pow(startValue, -2);
+            
+            }
+            return Math.Round(multiValue, 3);
+        }  
+    }
+}
---
+++ Tyuiu.KochetovKO.Sprint3.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KochetovKO.Sprint3.Task1.V5.Lib;
+namespace Tyuiu.KochetovKO.Sprint3.Task1.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            
+            double wait = 1;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
