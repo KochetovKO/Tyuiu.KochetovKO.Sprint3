e9310f1dd9d03f45758ff428aa8fb5b1943c8523
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-10-21 17:46:09 +0500

    Task2V19

--- Tyuiu.KochetovKO.Sprint3.Task2.V19.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint3.Task2.V19.Lib/DataService.cs
@@ -5,14 +5,14 @@ namespace Tyuiu.KochetovKO.Sprint3.Task2.V19.Lib
     {
         public double GetSumSeries(int startValue, int stopValue)
         {
-            double series = 1;
-
+            double series = 0;
+            int i = startValue;
             do
             {
-                series = series + Math.Cos(startValue) * 0.5;
-                startValue++;
+                series = series + (Math.Cos(i) * 0.5);
+                i++;
             
-            } while (startValue <= stopValue);
+            } while (i <= stopValue);
             return Math.Round(series, 3);
         }
     }
